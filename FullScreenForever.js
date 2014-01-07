294eae7422f2ff52c193fc66d5aa28b9e6019984
KobbyA <kobby,appiah@knockinc.com>
2014-01-07 11:33:36 -0600

    initial commit

---
+++ FullScreenForever.js
@@ -0,0 +1,81 @@
+/**
+ * FullScreenForever jQuery plugin
+ * makes an element (usually image) always fullscreen width to the window and in ratio
+ * Author: Kobby Appiah
+ * kobby@kobby-appiah.com
+ * @param opts
+ * @constructor
+ */
+$.fn.FullScreenForever = function(opts){
+
+    var $window = $(window);
+//    var wWidth = $window.width();
+//    var wHeight = $window.height();
+    var $el = this;
+    var $parent = $el.parent();
+    var pwidth = $parent.width();
+    var pHeight = $parent.height();
+
+    var defaults = {
+        useCSS : false,
+        useOverflow: false,
+        onResizeBefore : null,
+        onResizeAfter :null
+    };
+
+    if(opts) $.extend(defaults, opts);
+    if(defaults.useOverflow) $parent.css('overflow', 'hidden');
+
+    if(!$.FullScreenForeverInit){
+        $.FullScreenForeverInit = true;
+        $window.on('resize', function(){
+            $window.trigger('fullScreenForever');
+        });
+    }
+
+    $(window).bind('resize', function(){
+//        wWidth = $window.width();
+//        wHeight = $window.height();
+        pwidth = $parent.width();
+        pHeight = $parent.height();
+        callback(defaults.onResizeBefore, this);
+        scaleImage();
+        callback(defaults.onResizeAfter, this);
+    });
+
+    function callback(fn, ctx, args){
+        if(!args) args = {};
+        if(fn && typeof fn == "function"){
+            if(ctx){
+                fn.apply(ctx, [args]);
+            } else {
+                fn.apply(args);
+            }
+        }
+
+    }
+
+    function scaleImage() {
+        var ratio = Math.max(pwidth / $el.width(), pHeight / $el.height());
+        defaults.useCSS ? scaleByTransform(ratio) : scaleByWidthHeight(ratio);
+    }
+
+    function scaleByTransform(ratio) {
+        var transform = 'translate3d(' + [-($el.width()) / 2, -($el.height()) / 2, 0].join('px,') + 'px) scale3d(' + ratio + ',' + ratio + ',1) translate3d(' + [pwidth / 2 / ratio, pHeight / 2 / ratio, 0].join('px,') + ')';
+        $el.css({
+            "-moz-transform": transform,
+            "-ms-transform": transform,
+            "-o-transform": transform,
+            "-webkit-transform": transform,
+            "transform": transform
+        });
+    }
+
+    function scaleByWidthHeight(ratio) {
+        var _h = $el.height() * ratio, _w = $el.width() * ratio;
+        var left = (pwidth - _w) / 2, top = (pHeight - _h) / 2;
+        $el.css({width: _w + 'px', height: _h + "px", top: top + 'px', left: left + 'px'});
+    }
+};
+
+;
